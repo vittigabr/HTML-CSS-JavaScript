4663cc186873dcdfba262f367fc636b535246ad8
Gabriel Vitti <vittigabriel23@gmail.com>
2026-04-01 18:18:55 -0300

    Criação de uma nova pasta sobre estudos do js

---
+++ estudoJS/teste.js
@@ -0,0 +1,4 @@
+window.alert('Primeiro teste') // Cria um alerta na tela
+window.confirm('Segundo teste') // Cria uma pergunta de "sim ou não"
+window.prompt('Terceiro teste: escreva certo') // Cria um espaço de resposta
+
