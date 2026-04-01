abb54ba8a0b82a4420141daa93e7aabd6ffb3a72
Gabriel Vitti <vittigabriel23@gmail.com>
2026-04-01 18:59:40 -0300

    avanço até a aula 07

--- estudoJS/teste.js
+++ estudoJS/teste.js
@@ -1,4 +1,13 @@
+// Aula 04 - módulo A
 window.alert('Primeiro teste') // Cria um alerta na tela
 window.confirm('Segundo teste') // Cria uma pergunta de "sim ou não"
 window.prompt('Terceiro teste: escreva certo') // Cria um espaço de resposta
 
+// Aula 06 - módulo B
+let nome = window.prompt('Digite seu nome: ')
+window.alert('É um prazer te conhecer, '+ nome)
+
+let number1 = window.prompt('Digite um número: ')
+let number2 = window.prompt('Digite outro número: ')
+let soma = Number.parseInt(number1)+ Number.parseInt(number2) // Transforma em number
+window.alert('O resultado da soma é '+ soma)
