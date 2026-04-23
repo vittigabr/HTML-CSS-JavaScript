5f192361d8e375ad91e64e9bb222303cb8af512e
Gabriel Vitti <177020343+vittigabr@users.noreply.github.com>
2026-04-23 14:33:17 -0300

    fix

--- estudoJS/teste.js
+++ estudoJS/teste.js
@@ -5,9 +5,9 @@ window.prompt('Terceiro teste: escreva certo') // Cria um espaço de resposta
 
 // Aula 06 - módulo B
 let nome = window.prompt('Digite seu nome: ')
-window.alert('É um prazer te conhecer, '+ nome)
+document.writeln('É um prazer te conhecer, '+ nome)
 
 let number1 = window.prompt('Digite um número: ')
 let number2 = window.prompt('Digite outro número: ')
 let soma = Number.parseInt(number1)+ Number.parseInt(number2) // Transforma em number
-window.alert('O resultado da soma é '+ soma)
+window.alert(`O resultado da soma é ${soma}`)
