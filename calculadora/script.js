96c41cf2beaede39728ecde9cfcad79442f58aea
Gabriel Vitti <vittigabriel23@gmail.com>
2026-04-24 09:45:27 -0300

    Renan fez funcionar

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,1 +1,14 @@
-var color = document.getElementsByTagName('body')
+//let bnt = document.getElementsByTagName('button')[0].addEventListener('click'); function mudarCor() {}
+
+function changeColor(){
+    console.log("funciona")
+    let body = document.getElementById('body-div')
+    if(body.classList.contains("light-bg")){
+        body.classList.remove('light-bg')
+        body.classList.add('dark-bg')
+    }else{
+        body.classList.remove('dark-bg')
+        body.classList.add('light-bg')
+    }
+    
+}
