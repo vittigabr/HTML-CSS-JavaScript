a6d4c1354391cbb0222b0edd939d45ea8ac9cda6
Gabriel Vitti <vittigabriel23@gmail.com>
2026-05-06 19:52:39 -0300

    update

---
+++ estudoJS/lacos.js
@@ -0,0 +1,10 @@
+var c = 1
+/*while(c<7){
+    console.log('tudo bem?')
+    c++
+}*/
+
+do{
+    console.log('tudo bem sim')
+    c++
+}while(c<7)
---
+++ estudoJS/node_b5f129faa4ffd.tmp.js
@@ -0,0 +1,9 @@
+var c = 1
+/*while(c<7){
+    console.log('tudo bem?')
+    c++
+}*/
+
+do{
+    console.log('tudo bem sim')
+}while(c<7)
