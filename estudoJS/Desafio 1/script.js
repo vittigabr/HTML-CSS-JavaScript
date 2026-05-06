3b6f43219112ab76db01d2bd7aa48af9194f5429
Gabriel Vitti <vittigabriel23@gmail.com>
2026-05-05 21:08:26 -0300

    finalizaer o desafio 01

--- estudoJS/Desafio 1/script.js
+++ estudoJS/Desafio 1/script.js
@@ -1,6 +1,21 @@
 let agora = new Date()
-let hora = agora.getHours()
+var hora = agora.getHours()
 let minuto = agora.getMinutes()
 
 let hora_minuto = document.getElementById('horas')
 hora_minuto.innerHTML = `<p>Agora são ${hora}h${minuto}.</p>`
+
+let img = document.getElementById('imgs')
+
+if(hora >= 7 && hora < 12){
+    img.src = 'assets/manha.jpg'
+    document.body.style.background = '#ec6b01'
+}
+else if(hora >= 12 && hora < 18){
+    img.src = 'assets/tarde.jpg'
+    document.body.style.background = '#f0da12'
+}
+else{
+    img.src = 'assets/noite.jpg'
+    document.body.style.background = '#000'
+}
