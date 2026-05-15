a6bc12be5ffe50012343ec6dc800109ae7afd314
Gabriel Vitti <vittigabriel23@gmail.com>
2026-05-15 10:15:48 -0300

    tentativa

--- estudoJS/Desafio 3/script.js
+++ estudoJS/Desafio 3/script.js
@@ -1,20 +1,9 @@
-const res = document.querySelector('div#resultado')
-const botao = document.querySelector('input.botao')
-botao.addEventListener('click', () =>{
-    var inicio = document.querySelector('input.campo'[0])
-    var fim = document.querySelector('input.campo'[1])
-    var pulo = document.querySelector('input.campo'[2])
+const botao = document.getElementById('botao')
 
-    var inicio = inicio.value
-    var fim = fim.value
-    var pulo = pulo.value
+botao.addEventListener('click', verificar)
+function verificar(){
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var pulo = document.getElementById('pulo')
 
-    res.innerHTML = `${inicio}...`
-    inicio += pulo
-
-    while(inicio<=fim){
-        res.innerHTML += `${inicio}...`
-        inicio += pulo
-    }
-    res.innerHTML += 'FIM'
-})
+}
