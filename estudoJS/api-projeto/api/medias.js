68d2782f5725c623d69cba801c779f1093050980
Gabriel Vitti <vittigabriel23@gmail.com>
2026-05-22 08:15:55 -0300

    remoção como comentário

--- estudoJS/api-projeto/api/medias.js
+++ estudoJS/api-projeto/api/medias.js
@@ -1,30 +1,30 @@
-// const medias = [
-//     {
-//         id: 1,
-//         type: "movie",
-//         tittle: "Gigantes de Aço"
-//     },
-//     {
-//         id: 2,
-//         type: "serie",
-//         tittle: "MaxSteel"
-//     },
-//     {
-//         id: 3,
-//         type: "tv",
-//         tittle: "Ben 10"
-//     }
-// ];
+const medias = [
+    {
+        id: 1,
+        type: "movie",
+        tittle: "Gigantes de Aço"
+    },
+    {
+        id: 2,
+        type: "serie",
+        tittle: "MaxSteel"
+    },
+    {
+        id: 3,
+        type: "tv",
+        tittle: "Ben 10"
+    }
+];
 
-// module.exports = (req, res) => {
-//     if (req.method === "GET"){
-//         return res.status(200).json({medias});
-//     }
-//     else if(req.method === "POST"){
-//         const newMedia = req.body;
-//         return res.status(201).json([...medias, newMedia])
-//     }
-// }
+module.exports = (req, res) => {
+    if (req.method === "GET"){
+        return res.status(200).json({medias});
+    }
+    else if(req.method === "POST"){
+        const newMedia = req.body;
+        return res.status(201).json([...medias, newMedia])
+    }
+}
 
 const { getCollection } = require('./_lib/mongo');
 const { validateProduct } = require('./_lib/validators/media');
