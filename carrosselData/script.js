4fac3f36a035bc1c49b6e85ffec993a4f7a38e38
Gabriel Vitti <vittigabriel23@gmail.com>
2026-06-09 10:40:15 -0300

    atualização do outro arquivo

--- carrosselData/script.js
+++ carrosselData/script.js
@@ -1,71 +1,24 @@
-const botaoA = document.getElementById('bntA')
-const botaoP = document.getElementById('bntP')
-// const semanas = document.querySelectorAll('.semana')
-// const semana2 = document.querySelector('.semana2')
-// const semana3 = document.querySelector('.semana3')
-// const semana4 = document.querySelector('.semana4')
-let cont = 1
+const bntP = document.getElementById('bntP')
+const bntA = document.getElementById('bntA')
+let linkP = document.getElementById('linkP')
+let linkA = document.getElementById('linkA')
+const semanas = document.querySelectorAll('.semana')
+let limite = semanas.length
+let contador = 1
 
-// Primeira tentativa
-// botaoP.addEventListener('click', () => {
-//     const semanaSelecionada = document.querySelector('.semana.selecionada')
-//     semanaSelecionada.classList.remove('selecionada')
-//     semanas.forEach((indice) => {
-//         semanas[indice].classList.add('.selecionada')
-//     });
-// })
+bntP.addEventListener('click', proximo)
+bntA.addEventListener('click', anterior)
 
-// Segunda tentativa
-// botaoP.addEventListener('click', () => {
-//     const semanaSelecionada = document.querySelector('.semana1.selecionada')
-//     if(cont==1){
-//         cont++
-//         semanaSelecionada.classList.remove('.selecionada')
-//         semana2.classList.add('.selecionada')
-//     }
-//     else if(cont==2){
-//         cont++
-//         semanaSelecionada.classList.remove('.selecionada')
-//         semana3.classList.add('.selecionada')
-//     }
-//     else if(cont==3){
-//         cont++
-//         semanaSelecionada.classList.remove('.selecionada')
-//         semana4.classList.add('.selecionada')
-//     }
-//     else{
-//         cont = 1
-//     }
-// })
+function proximo(){
+    if(contador<limite){
+        contador++
+        linkP.setAttribute('href', `#semana${contador}`)
+    }
+}
 
-botaoP.addEventListener('click', () => {
-    const semanaSelecionada = document.querySelector('.selecionada')
-    if(cont==1){
-        cont = 2
-        semanaSelecionada.style.marginLeft = '-25.9%'
+function anterior(){
+    if(contador>1){
+        contador--
+        linkA.setAttribute('href', `#semana${contador}`)
     }
-    else if(cont==2){
-        cont = 3
-        semanaSelecionada.style.marginLeft = '-107.6%'
-    }
-    else{
-        cont = 4
-        semanaSelecionada.style.marginLeft = '-516%'
-    }
-})
-
-botaoA.addEventListener('click', () => {
-    const semanaSelecionada = document.querySelector('.selecionada')
-    if(cont==4){
-        cont = 3
-        semanaSelecionada.style.marginLeft = '-107.6%'
-    }
-    else if(cont==3){
-        cont = 2
-        semanaSelecionada.style.marginLeft = '-25.9%'
-    }
-    else{
-        cont = 1
-        semanaSelecionada.style.marginLeft = 'auto'
-    }
-})
+}
