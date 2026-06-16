95157bb6e514df8ae140c7fd378f91c42b429ecc
Gabriel Vitti <vittigabriel23@gmail.com>
2026-06-16 20:02:29 -0300

    feat create file começando mais um tipo de site para fixação

---
+++ Site Bijuus/script.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll('.botao')
+
+botoes.forEach((botao, index) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.botao.selecionado')
+        botaoSelecionado.classList.remove('selecionado')
+        botao.classList.add('selecionado')
+    })
+})
