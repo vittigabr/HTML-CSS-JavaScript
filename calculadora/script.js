50f28711c342baf24b37208fc94eb3620896628b
Gabriel Vitti <vittigabriel23@gmail.com>
2026-06-18 14:25:47 -0300

    fix criação dos botoes comm funcionalidade

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,30 +1,28 @@
-//let bnt = document.getElementsByTagName('button')[0].addEventListener('click'); function mudarCor() {}
+let visor = document.getElementById('resposta')
+const botoes = document.querySelectorAll('.bnt')
+const bSoma = document.getElementById('botaoSoma')
+const bSubtra = document.getElementById('botaoSubtracao')
+const bMulti = document.getElementById('botaoMulti')
+const bDivisao = document.getElementById('botaoDivisao')
 
-/*function changeColor(){
-    console.log("funciona")
-    let body = document.getElementById('body-div')
-    if(body.classList.contains("light-bg")){
-        body.classList.remove('light-bg')
-        body.classList.add('dark-bg')
-    }else{
-        body.classList.remove('dark-bg')
-        body.classList.add('light-bg')
-    }
-    
-}*/
+botoes.forEach((botao, index) => {
+    botao.addEventListener('click', () => {
+        visor.innerText += botoes[index].getHTML()
+    })
+})
 
-var botao = document.querySelector('#bnt')
-botao.addEventListener('click', changeColor())
+bSoma.addEventListener('click', () => {
+    visor.innerText += '+'
+})
 
-function changeColor(){
-    console.log('funciona')
-    var back = document.querySelector('.body-div')
-    if (back.classList.contains('light-theme')){
-        back.classList.remove('light-theme');
-        back.classList.add('dark-theme');
-    }
-    else{
-        back.classList.remove('dark-theme');
-        back.classList.add('light-theme');
-    }
-}
+bSubtra.addEventListener('click', () => {
+    visor.innerText += '-'
+})
+
+bMulti.addEventListener('click', () => {
+    visor.innerText += '*'
+})
+
+bDivisao.addEventListener('click', () => {
+    visor.innerText += '/'
+})
