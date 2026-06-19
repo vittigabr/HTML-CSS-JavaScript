088c538cbf1b0fffbba3bae8fa9787795f5763d6
Gabriel Vitti <vittigabriel23@gmail.com>
2026-06-19 17:54:41 -0300

    finish/ finalizado até o momento a calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -7,12 +7,14 @@ const bDivisao = document.getElementById('botaoDivisao')
 const bIgual = document.getElementById('equal')
 const bCancel = document.getElementById('cancela')
 const bApaga = document.getElementById('clear')
+const bPorcent = document.getElementById('porcentagem')
+const bColor = document.getElementById('color')
 let numero1 = 0
 let numero2 = 0
 
 botoes.forEach((botao, index) => {
     botao.addEventListener('click', () => {
-        visor.innerText += botoes[index].getHTML()
+        visor.innerText += botoes[index].innerText
     })
 })
 
@@ -32,34 +34,49 @@ bDivisao.addEventListener('click', () => {
     visor.innerText += '/'
 })
 
+bPorcent.addEventListener('click', () => {
+    visor.innerText += '%'
+})
+
 bIgual.addEventListener('click', () => {
-    let txtNumero = visor.innerText
-    if(txtNumero.indexOf('+') != -1){
-        numero1 = Number(txtNumero.split('+')[0])
-        numero2 = Number(txtNumero.split('+')[1])
-        let soma = numero1 + numero2
-        visor.innerText = soma
+    // let txtNumero = visor.innerText
+    // if(txtNumero.indexOf('+') != -1){
+    //     numero1 = Number(txtNumero.split('+')[0])
+    //     numero2 = Number(txtNumero.split('+')[1])
+    //     let soma = numero1 + numero2
+    //     visor.innerText = soma
+    // }
+    // else if(txtNumero.indexOf('-') != -1){
+    //     numero1 = Number(txtNumero.split('-')[0])
+    //     numero2 = Number(txtNumero.split('-')[1])
+    //     let subtracao = numero1 - numero2
+    //     visor.innerText = subtracao
+    // }
+    // else if(txtNumero.indexOf('*') != -1){
+    //     numero1 = Number(txtNumero.split('*')[0])
+    //     numero2 = Number(txtNumero.split('*')[1])
+    //     let multiplicacao = numero1 * numero2
+    //     visor.innerText = multiplicacao
+    // }
+    // else if(txtNumero.indexOf('/') != -1){
+    //     numero1 = Number(txtNumero.split('/')[0])
+    //     numero2 = Number(txtNumero.split('/')[1])
+    //     let divisao = numero1 / numero2
+    //     visor.innerText = divisao
+    // }
+    // else if(txtNumero.indexOf('%') != -1){
+    //     numero1 = Number(txtNumero.split('%')[0])
+    //     let porcentagem = numero1/100
+    //     visor.innerText = porcentagem
+    // }
+    // else{
+    //     visor.innerText = 'Error'
+    // }
+    try{
+        visor.innerText = eval(visor.innerText)
     }
-    else if(txtNumero.indexOf('-') != -1){
-        numero1 = Number(txtNumero.split('-')[0])
-        numero2 = Number(txtNumero.split('-')[1])
-        let subtracao = numero1 - numero2
-        visor.innerText = subtracao
-    }
-    else if(txtNumero.indexOf('*') != -1){
-        numero1 = Number(txtNumero.split('*')[0])
-        numero2 = Number(txtNumero.split('*')[1])
-        let multiplicacao = numero1 * numero2
-        visor.innerText = multiplicacao
-    }
-    else if(txtNumero.indexOf('/') != -1){
-        numero1 = Number(txtNumero.split('/')[0])
-        numero2 = Number(txtNumero.split('/')[1])
-        let divisao = numero1 / numero2
-        visor.innerText = divisao
-    }
-    else{
-        visor.innerText = 'Error'
+    catch{
+        visor.innerText = 'Erro'
     }
 })
 
@@ -71,4 +88,31 @@ bApaga.addEventListener('click', () => {
     let txtNumero = visor.innerText.split('')
     txtNumero.pop()
     visor.innerText = txtNumero.join('')
+})
+
+bColor.addEventListener('click', () => {
+    let background = document.body.classList
+    const botoesEspeciais = document.querySelectorAll('.botaoEspecial-light')
+    if(background.contains('lightTheme')){
+        background.remove('lightTheme')
+        background.add('darkTheme')
+        botoes.forEach((botao, index) => {
+            botao.classList.add('bnt-dark')
+        })
+        botoesEspeciais.forEach((botao) => {
+            botao.classList.add('botaoEspecial-dark')
+        })
+        bColor.innerHTML = '&#127774;'
+    }
+    else{
+        background.remove('darkTheme')
+        background.add('lightTheme')
+        botoes.forEach((botao, index) => {
+            botao.classList.remove('bnt-dark')
+        })
+        botoesEspeciais.forEach((botao) => {
+            botao.classList.remove('botaoEspecial-dark')
+        })
+        bColor.innerHTML = '&#127769;'
+    }
 })
