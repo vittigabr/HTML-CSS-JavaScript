ec380a06bb78877c615c68b361aa5aaccdc3ded0
Gabriel Vitti <vittigabriel23@gmail.com>
2026-06-21 17:00:15 -0300

    feat/ construção do jogo da velha e início da lógica

--- Jogo da Velha/script.js
+++ Jogo da Velha/script.js
@@ -1,26 +1,58 @@
-const escolha = document.querySelectorAll(".choosed");
+const placarX = document.getElementById('placarX')
+const placarO = document.getElementById('placarO')
+const casas = document.querySelectorAll('.casa')
+let current = 0
+let px = 0
+let po = 0
 
-// forEach é para selecionar o elemento específico dentro da lista criada pelo querySelectorAll
-escolha.forEach(item => {
-    item.addEventListener('click', clicar)
+placarX.innerText = px++
+placarO.innerText = po++
+
+function zerarCasas(){
+    setTimeout(() => {
+        for(let c = 0; c<=casas.length; c++){
+            casas[c].innerText = ''
+        }
+    }, 1000);
+}
+
+casas.forEach((casa, index) => {
+    casa.addEventListener('click', () => {
+        if(current==0){
+            casa.innerText = 'X'
+            current++
+        }
+        else{
+            casa.innerText = 'O'
+            current--
+        }
+
+        // vence somente o X para teste - deu certo
+        if(casas[0].innerText=='X' && casas[1].innerText=='X' && casas[2].innerText=='X' || casas[0]){
+            placarX.innerText = px++
+            zerarCasas()
+        }
+        else if(casas[0].innerText=='X' && casas[1].innerText=='X' && casas[2].innerText=='X'){
+            placarX.innerText = px++
+            zerarCasas()
+        }
+        else if(casas[0].innerText=='X' && casas[1].innerText=='X' && casas[2].innerText=='X'){
+            placarX.innerText = px++
+            zerarCasas()
+        }
+        else if(casas[0].innerText=='X' && casas[1].innerText=='X' && casas[2].innerText=='X'){
+            placarX.innerText = px++
+            zerarCasas()
+        }
+        else if(casas[0].innerText=='X' && casas[1].innerText=='X' && casas[2].innerText=='X'){
+            placarX.innerText = px++
+            zerarCasas()
+        }
+        else if(casas[0].innerText=='X' && casas[1].innerText=='X' && casas[2].innerText=='X'){
+            placarX.innerText = px++
+            zerarCasas()
+        }
+
+    })
 })
 
-function clicar(event){
-    event.target.innerText = 'X'
-}
-
-// funcionou a troca de tema do fundo **só precisa adicionar mais config para mudar todos os elementos do site
-const botao = document.getElementById('bnt')
-const back = document.body.classList
-
-botao.addEventListener('click', changeColor)
-
-function changeColor(){
-    // console.log('funciona')
-    if(back.contains('darkBody')){
-        back.remove('darkBody')
-    }
-    else{
-        back.add('darkBody')
-    }
-}
