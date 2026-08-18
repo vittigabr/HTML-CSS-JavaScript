b07c219fc865a515510dbfa740328a57057cd078
Gabriel Vitti <177020343+vittigabr@users.noreply.github.com>
2026-08-18 14:33:00 -0300

    fix/ tentativa de pegar endereço

---
+++ index.js
@@ -0,0 +1,8 @@
+let endereco = document.getElementById('endereco')
+let enderecos = endereco.value
+
+let botao = document.getElementById('botao')
+
+botao.addEventListener('click', () =>{
+    console.log(enderecos)
+})
