a626aef6da3b955b0616bc2fbc854a4d26177da7
Gabriel Vitti <177020343+vittigabr@users.noreply.github.com>
2026-08-19 14:36:16 -0300

    fix/ tentativa numero 1

---
+++ atividade-aprimorada/script2.js
@@ -0,0 +1,97 @@
+const botaoBuscar = document.getElementById("buscar");
+
+botaoBuscar.addEventListener("click", buscarClima);
+
+function buscarClima() {
+
+    const cidade = document.getElementById("cidade").value.trim();
+
+    if (cidade === "") {
+        alert("Digite o nome de uma cidade.");
+        return;
+    }
+
+    const chaveApi = "6524a1ef013cd74821010118ec46574f";
+
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveApi}&units=metric&lang=pt_br`;
+
+    fetch(url)
+
+        function calcularSunRise(){
+            const timezone = dados.timezone
+            const sunrise = dados.sys.sunrise
+
+            const timestampLocal = (sunrise + timezone) * 1000;
+            const dataLocal = new Date(timestampLocal);
+
+            const hora = dataLocal.getUTCHours().toString().padStart(2, '0');
+            const minuto = dataLocal.getUTCMinutes().toString().padStart(2, '0');
+
+            document.getElementById('horaSunRise').textContent = hora
+            document.getElementById('minutoSunRise').textContent = minuto
+        }
+
+        .then(function(resposta) {
+
+            if (!resposta.ok) {
+                throw new Error("Cidade não encontrada.");
+            }
+
+            return resposta.json();
+        })
+
+        .then(function(dados) {
+
+            console.log(dados);
+
+            document.getElementById("nomeCidade").textContent =
+                dados.name;
+
+            document.getElementById("temperaturaMin").textContent =
+                dados.main.temp_min.toFixed(1);
+
+            document.getElementById("temperaturaMax").textContent =
+                dados.main.temp_max.toFixed(1);
+
+            document.getElementById("sensacao").textContent =
+                dados.main.feels_like.toFixed(1);
+
+            document.getElementById("umidade").textContent =
+                dados.main.humidity;
+
+            calcularSunRise()
+
+            function calcularSunSet(){
+                const timezone = dados.timezone
+                const sunrise = dados.sys.sunset
+
+                const timestampLocal = (sunrise + timezone) * 1000;
+                const dataLocal = new Date(timestampLocal);
+
+                const hora = dataLocal.getUTCHours().toString().padStart(2, '0');
+                const minuto = dataLocal.getUTCMinutes().toString().padStart(2, '0');
+
+                document.getElementById('horaSunRise').textContent = hora
+                document.getElementById('minutoSunRise').textContent = minuto
+            }
+            calcularSunSet()
+
+            const rajadaKmH = dados.wind.gust *3.6;
+
+            const ventoKmH = dados.wind.speed * 3.6;
+
+            document.getElementById("vento").textContent =
+                ventoKmH.toFixed(1);
+            
+            document.getElementById("rajada").textContent =
+                rajadaKmH.toFixed(1);
+        })
+
+        .catch(function(erro) {
+
+            console.log("Erro:", erro);
+
+            alert("Não foi possível encontrar a cidade.");
+        });
+}
