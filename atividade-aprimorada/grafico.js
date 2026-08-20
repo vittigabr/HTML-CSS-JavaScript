787136829001a7c64fa44ee3c3f7ca83936e9ff3
Gabriel Vitti <177020343+vittigabr@users.noreply.github.com>
2026-08-20 14:31:20 -0300

    fix/ fazer o gráfico

--- atividade-aprimorada/grafico.js
+++ atividade-aprimorada/grafico.js
@@ -37,3 +37,27 @@ export function previsaoDias(latitude, longitude, chaveApi) {
 
 // construção do gráfico 
 
+const ctx = document.getElementById('chart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+        label: '# of Votes',
+        data: lista,
+        borderWidth: 3,
+        backgroundColor: ['red', 'blue', 'yellow', 'green', 'purple', 'orange'],
+        pointBorderWidth: 9,
+        pointBorderColor: ['red', 'blue', 'yellow', 'green', 'purple', 'orange']
+        }]
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: false
+        }
+        },
+        
+    }
+});
