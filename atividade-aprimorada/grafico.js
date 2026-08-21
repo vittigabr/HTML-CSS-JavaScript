6e99e4645466b2d309f2068b3f9ee78eb2fd9e0a
Gabriel Vitti <vittigabriel23@gmail.com>
2026-08-21 13:47:02 -0300

    / finalização do sistema

--- atividade-aprimorada/grafico.js
+++ atividade-aprimorada/grafico.js
@@ -1,5 +1,6 @@
 // coleta das informações do gráfico
-let grafico = null
+let graficoTemperatura = null
+let graficoChuva = null
 let dias = new Date
 let diaSemana = dias.getUTCDay()
 let semana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
@@ -7,7 +8,8 @@ let cores = []
 
 export function previsaoDias(latitude, longitude, chaveApi) {
 
-    let lista = []
+    let temperaturas = []
+    let chuvas = []
     const keyApi = chaveApi
     const lat = Number(latitude)
     const lon = Number(longitude)
@@ -24,14 +26,21 @@ export function previsaoDias(latitude, longitude, chaveApi) {
         })
 
         .then(function(previsao) {
-            lista = [];
+            temperaturas = [];
+            chuvas = [];
 
             if (previsao && previsao.list) {
                 
                 for (let i = 0; i < 40; i+=8) {
                     
                     if (previsao.list[i] && previsao.list[i].main) {
-                        lista.push(previsao.list[i].main.temp);
+                        temperaturas.push(previsao.list[i].main.temp);
+                        const chuva = previsao.list[i].rain
+                        ? previsao.list[i].rain["3h"]
+                        : 0;
+
+                        document.getElementById("chuvaMM").textContent = chuva
+                        chuvas.push(chuva)
                         if(previsao.list[i].main.temp >= 25){
                             cores.push('red')
                         }
@@ -58,22 +67,24 @@ export function previsaoDias(latitude, longitude, chaveApi) {
                     semanas.push(semana[diaSemana], semana[diaSemana - 6], semana[diaSemana - 5], semana[diaSemana - 4], semana[diaSemana - 3])
                 }
 
-                const ctx = document.getElementById('chart');
+                const ctxTemperatura = document.getElementById('graficoTemperatura');
 
-                if (grafico) {
-                    grafico.destroy(); // remove o gráfico anterior
+                if (graficoTemperatura) {
+                    graficoTemperatura.destroy(); // remove o gráfico anterior
                 }
 
-                grafico = new Chart(ctx, {
+                graficoTemperatura = new Chart(ctxTemperatura, {
                     type: 'line',
                     data: {
                         labels: semanas,
                         datasets: [{
                             label: 'Temperatura',
-                            data: lista,
+                            data: temperaturas,
+                            borderColor: 'black',
                             borderWidth: 3,
                             pointBorderWidth: 9,
-                            pointBorderColor: cores
+                            pointBorderColor: cores,
+                            backgroundColor: 'red'
                         }]
                     },
                     options: {
@@ -82,20 +93,64 @@ export function previsaoDias(latitude, longitude, chaveApi) {
                             beginAtZero: false
                         }
                         },
-                        
-                    }
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    color: 'red'
+                                }
+                            }
+                        },
+                    },
+                });
+
+                const ctxChuva = document.getElementById('graficoChuva');
+
+                if (graficoChuva) {
+                    graficoChuva.destroy(); // remove o gráfico anterior
+                }
+
+                graficoChuva = new Chart(ctxChuva, {
+                    type: 'line',
+                    data: {
+                        labels: semanas,
+                        datasets: [{
+                            label: 'Chuva',
+                            data: chuvas,
+                            borderColor: 'black',
+                            borderWidth: 3,
+                            pointBorderWidth: 9,
+                            pointBorderColor: cores,
+                            backgroundColor: 'blue'
+                        }]
+                    },
+                    options: {
+                        scales: {
+                        y: {
+                            beginAtZero: false
+                        }
+                        },
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    color: 'blue'
+                                }
+                            }
+                        },
+                    },
                 });
             }
             
-            console.log(lista)
+            console.log(temperaturas)
+            console.log(chuvas)
 
-            lista = []
+            temperaturas = []
+            chuvas = []
         })
 }
 
 // construção do gráfico 
 
-// const ctx = document.getElementById('chart');
+// const ctxTemperatura = document.getElementById('chart');
 
 // new Chart(ctx, {
 //     type: 'line',
