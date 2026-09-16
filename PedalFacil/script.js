b5b0ba0986e14a8b89502f37a08e0166edac795d
Gabriel Vitti <177020343+vittigabr@users.noreply.github.com>
2026-09-16 14:26:00 -0300

    fix/ tentativa de script

--- PedalFacil/script.js
+++ PedalFacil/script.js
@@ -36,3 +36,35 @@ botaoBike.forEach((botao, index) => {
         }
     })
 })
+
+
+
+// Contagem de dias de aluguel
+const dataRetirada = document.getElementById('dataRetirada')
+const dataDevolucao = document.getElementById('dataDevolucao')
+const botaoConfirmar = document.getElementById('botaoConfirmar')
+
+const mes31 = ['01', '03', '05', '07', '08', '10', '12']
+const mes30 = ['04', '06', '09', '11']
+
+    //a a a a - m m - d d
+    //0 1 2 3 4 5 6 7 8 9  
+
+botaoConfirmar.addEventListener('click', () => {
+    let contadorDias = 0
+    const diaRetirada = dataRetirada.value[8] + dataRetirada.value[9]
+    const mesRetirada = dataRetirada.value[5] + dataRetirada.value[6]
+
+    const diaDevolucao = dataDevolucao.value[8] + dataDevolucao.value[9]
+    const mesDevolucao = dataDevolucao.value[5] + dataDevolucao.value[6]
+
+    if(mesRetirada in mes31){
+        contadorDias = Number(diaDevolucao) + (31 - Number(diaRetirada))
+        console.log(contadorDias)
+    }
+    else{
+        contadorDias = Number(diaDevolucao) + (30 - Number(diaRetirada))
+        console.log(contadorDias)   
+    }
+
+})
